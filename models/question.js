6b153bde37955794615100998dd3205913267a64
broliver18 <bruno.olive504@gmail.com>
2023-07-16 16:33:23 -0400

    Modify question schema

--- models/question.js
+++ models/question.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema; 
 
 const questionSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
     questionType: {
         type: String,
         required: true
