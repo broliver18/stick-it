983cffb251b80a25ca5695b0d05ee6fb7fd9f04d
broliver18 <bruno.olive504@gmail.com>
2023-07-17 16:05:53 -0400

    Add question check before going through database

--- server.js
+++ server.js
@@ -13,11 +13,31 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  socket.on('display-info', (nameInput, pinInput) => {
+  socket.on("display-info", (nameInput, pinInput) => {
     console.log(nameInput);
     console.log(pinInput);
-  })
-})
+  });
+
+  socket.on("quiz-info", (questions) => {
+    if (
+      questions.find(
+        (questionInfo) =>
+          (questionInfo.questionType === "short-answer" &&
+            (!questionInfo.question || !questionInfo.shortAnswer)) ||
+          (questionInfo.questionType === "multiple-choice" &&
+            (!questionInfo.question ||
+              !questionInfo.answerOne ||
+              !questionInfo.answerTwo ||
+              !questionInfo.answerThree ||
+              !questionInfo.answerFour ||
+              !questionInfo.correctAnswer))
+      )
+    ) {
+      const message = "Please fill out all required input fields.";
+      socket.emit("error-message", message);
+    }
+  });
+});
 
 const dbURI = `mongodb+srv://brunoolive504:562412504$BMo@stick-it.6mxliys.mongodb.net/stick-it?retryWrites=true&w=majority`;
 mongoose
@@ -25,7 +45,9 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then((result) => server.listen(4000, () => {
-    console.log("Listening on port 4000");
-  }))
+  .then((result) =>
+    server.listen(4000, () => {
+      console.log("Listening on port 4000");
+    })
+  )
   .catch((err) => console.log(err));
