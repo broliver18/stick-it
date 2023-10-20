985fc16b5db42be4d17f24b1061654806199cba8
broliver18 <bruno.olive504@gmail.com>
2023-10-20 17:49:34 -0400

    Set up redis for ubuntu server

--- redis.js
+++ redis.js
@@ -1,5 +1,14 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const Redis = require("ioredis");
 
-const redisClient = new Redis(6379);
+const redisClient = new Redis({ 
+    port: 6379,
+    host: "162.243.175.89",
+    username: "default",
+    password: process.env.REDIS_PASSWORD, 
+});
 
 module.exports = redisClient;
