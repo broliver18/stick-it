32ce4a96a0d68f12e3c3f4102d905924e539a67b
broliver18 <bruno.olive504@gmail.com>
2023-10-20 18:15:45 -0400

    Configure redis for production

--- redis.js
+++ redis.js
@@ -1,5 +1,14 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const Redis = require("ioredis");
 
-const redisClient = new Redis();
+const redisClient = new Redis({
+  port: 6379,
+  host: "127.0.0.1",
+  username: "default",
+  password: process.env.REDIS_PASSWORD,
+});
 
 module.exports = redisClient;
